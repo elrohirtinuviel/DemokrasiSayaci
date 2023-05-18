91f186c722911c8e6dcf916a5f1be431677ab1b1
Emre <Emre@DESKTOP-0O82E1K>
2023-05-18 19:28:15 +0300

    New features

--- DemokrasiSayaci/DemokrasiSayaci.Android/MainActivity.cs
+++ DemokrasiSayaci/DemokrasiSayaci.Android/MainActivity.cs
@@ -4,10 +4,12 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using AndroidX.AppCompat.App;
+
 
 namespace DemokrasiSayaci.Droid
 {
-    [Activity(Label = "DemokrasiSayaci", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "DemokrasiSayaci", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize ,ScreenOrientation =ScreenOrientation.Portrait )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -16,8 +18,10 @@ namespace DemokrasiSayaci.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            Window.SetStatusBarColor(Android.Graphics.Color.Transparent);
+            Window.SetStatusBarColor(Android.Graphics.Color.ParseColor("#111111"));
             LoadApplication(new App());
+            
+            
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
--- DemokrasiSayaci/DemokrasiSayaci/MainPage.xaml.cs
+++ DemokrasiSayaci/DemokrasiSayaci/MainPage.xaml.cs
@@ -13,17 +13,13 @@ namespace DemokrasiSayaci
     {
         public MainPage()
         {
-            InitializeComponent();
+            InitializeComponent();           
             crateName = Preferences.Get("crateName", "-");
             if (crateName == "-" || crateName == null || crateName == "")
             {
                 SetCrateName();
             }
-            rteCount = Convert.ToInt32(Preferences.Get("rteCount", "0"));
-            kkCount = Convert.ToInt32(Preferences.Get("kkCount", "0"));
-            rteCounter.Text = rteCount.ToString();
-            kkCounter.Text = kkCount.ToString();
-            lblCrateName.Text = crateName;
+            SetValues();
         }
 
         public async void SetCrateName()
@@ -48,8 +44,60 @@ namespace DemokrasiSayaci
                 }
                 lblCrateName.Text = newText;
             }
+            SetTotalVote();
         }
 
+        public async void SetTotalVote()
+        {
+            try
+            {
+                totalVotes = Convert.ToInt32(await DisplayPromptAsync("UYARI", "Lütfen Kullanılan Oy Sayısını Giriniz:", "Onayla", "İptal"));
+                Preferences.Set("totalVotes", totalVotes.ToString());
+                currentVote = totalVotes;
+                Preferences.Set("currentVote", currentVote.ToString());
+                lblVotesLeft.Text = currentVote.ToString();
+            }
+            catch
+            {
+                SetTotalVote();
+            }
+            Preferences.Set("crateName", crateName);
+            lblCrateName.Text = crateName;
+            if (lblCrateName.Text == "-" || lblCrateName.Text == "")
+            {
+                SetCrateName();
+            }
+            else if (lblCrateName.Text == null)
+            {
+                System.Diagnostics.Process.GetCurrentProcess().Kill();
+            }
+            else if (lblCrateName.Text.Length >= 49)
+            {
+                string newText = "";
+                for (int i = 0; i < 49; i++)
+                {
+                    newText += lblCrateName.Text[i];
+                }
+                lblCrateName.Text = newText;
+            }
+
+            //SetValues();
+        }
+
+        public void SetValues()
+        {
+            rteCount = Convert.ToInt32(Preferences.Get("rteCount", "0"));
+            kkCount = Convert.ToInt32(Preferences.Get("kkCount", "0"));
+            vibrate = Convert.ToBoolean(Preferences.Get("vibrate", "true"));
+            invalidVotes = Convert.ToInt32(Preferences.Get("invalidVotes", "0"));
+            currentVote = Convert.ToInt32(Preferences.Get("currentVote", "0"));          
+            totalVotes = Convert.ToInt32(Preferences.Get("totalVotes", "0"));
+            rteCounter.Text = rteCount.ToString();
+            kkCounter.Text = kkCount.ToString();
+            lblCrateName.Text = crateName;
+            lblInvalidVotes.Text = invalidVotes.ToString();
+            lblVotesLeft.Text = currentVote.ToString();
+        }
 
 
 
@@ -57,14 +105,42 @@ namespace DemokrasiSayaci
         TimeSpan increaseDuration = TimeSpan.FromMilliseconds(175);
         TimeSpan decreaseDuration = TimeSpan.FromMilliseconds(350);
         string crateName;
+        bool vibrate;
+        int totalVotes;
+        int currentVote;
+        int invalidVotes;
 
         //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
         private void buttonRTE_Clicked(object sender, EventArgs e)
         {
-            rteCount++;
-            rteCounter.Text = rteCount.ToString();
-            Vibration.Vibrate(increaseDuration);
-            Preferences.Set("rteCount", rteCount.ToString());
+            if (rteCount < totalVotes && currentVote > 0)
+            {
+                rteCount++;
+                rteCounter.Text = rteCount.ToString();
+                if (vibrate)
+                {
+                    Vibration.Vibrate(increaseDuration);
+                }
+                Preferences.Set("rteCount", rteCount.ToString());
+                currentVote--;
+                Preferences.Set("currentVote", currentVote.ToString());
+                lblVotesLeft.Text = currentVote.ToString();
+                if(rteCount > kkCount)
+                {
+                    rteCounter.TextColor = Color.Green;
+                    kkCounter.TextColor = Color.White;
+                }
+                else if (kkCount == rteCount)
+                {
+                    kkCounter.TextColor = Color.White;
+                    rteCounter.TextColor = Color.White;
+                }
+                else
+                {
+                    rteCounter.TextColor = Color.White;
+                }
+            }
+            
         }
 
         private void buttonRTEDecrease_Clicked(object sender, EventArgs e)
@@ -73,8 +149,29 @@ namespace DemokrasiSayaci
             {
                 rteCount--;
                 rteCounter.Text = rteCount.ToString();
-                Vibration.Vibrate(decreaseDuration);
+                if (vibrate)
+                {
+                    Vibration.Vibrate(decreaseDuration);
+                }
                 Preferences.Set("rteCount", rteCount.ToString());
+                currentVote++;
+                Preferences.Set("currentVote", currentVote.ToString());
+                lblVotesLeft.Text = currentVote.ToString();
+                if (rteCount > kkCount)
+                {
+                    rteCounter.TextColor = Color.Green;
+                    kkCounter.TextColor = Color.White;
+                }
+                else if (kkCount == rteCount)
+                {
+                    kkCounter.TextColor = Color.White;
+                    rteCounter.TextColor = Color.White;
+                }
+                else
+                {
+                    rteCounter.TextColor = Color.White;
+                    kkCounter.TextColor = Color.Green;
+                }
             }
 
         }
@@ -83,10 +180,34 @@ namespace DemokrasiSayaci
 
         private void buttonKK_Clicked(object sender, EventArgs e)
         {
-            kkCount++;
-            kkCounter.Text = kkCount.ToString();
-            Vibration.Vibrate(increaseDuration);
-            Preferences.Set("kkCount", kkCount.ToString());
+            if(kkCount < totalVotes && currentVote > 0)
+            {
+                kkCount++;
+                kkCounter.Text = kkCount.ToString();
+                if (vibrate)
+                {
+                    Vibration.Vibrate(increaseDuration);
+                }
+                Preferences.Set("kkCount", kkCount.ToString());
+                currentVote--;
+                Preferences.Set("currentVote", currentVote.ToString());
+                lblVotesLeft.Text = currentVote.ToString();
+                if (kkCount > rteCount)
+                {
+                    kkCounter.TextColor = Color.Green;
+                    rteCounter.TextColor = Color.White;
+                }
+                else if(kkCount == rteCount)
+                {
+                    kkCounter.TextColor = Color.White;
+                    rteCounter.TextColor = Color.White;
+                }
+                else
+                {
+                    kkCounter.TextColor = Color.White;
+                }
+            }
+           
         }
 
         private void buttonKKDecrease_Clicked(object sender, EventArgs e)
@@ -95,11 +216,35 @@ namespace DemokrasiSayaci
             {
                 kkCount--;
                 kkCounter.Text = kkCount.ToString();
-                Vibration.Vibrate(decreaseDuration);
+                if (vibrate)
+                {
+                    Vibration.Vibrate(decreaseDuration);
+                }
                 Preferences.Set("kkCount", kkCount.ToString());
+                currentVote++;
+                Preferences.Set("currentVote", currentVote.ToString());
+                lblVotesLeft.Text = currentVote.ToString();
+                if (kkCount > rteCount)
+                {
+                    kkCounter.TextColor = Color.Green;
+                    rteCounter.TextColor = Color.White;
+                }
+                else if (kkCount == rteCount)
+                {
+                    kkCounter.TextColor = Color.White;
+                    rteCounter.TextColor = Color.White;
+                }
+                else
+                {
+                    kkCounter.TextColor = Color.White;
+                    rteCounter.TextColor= Color.Green;
+                }
             }
 
         }
+
+
+
 
         //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -111,20 +256,58 @@ namespace DemokrasiSayaci
                 Preferences.Set("crateName", "-");
                 Preferences.Set("rteCount", "0");
                 Preferences.Set("kkCount", "0");
+                Preferences.Set("totalVotes", "0");
+                Preferences.Set("currentVotes", "0");
+                Preferences.Set("invalidVotes", "0");
                 rteCount = Convert.ToInt32(Preferences.Get("rteCount", "0"));
                 kkCount = Convert.ToInt32(Preferences.Get("kkCount", "0"));
                 lblCrateName.Text = "-";
+                lblVotesLeft.Text = "0";
+                lblInvalidVotes.Text = "0";
                 crateName = "";
                 rteCounter.Text = rteCount.ToString();
                 kkCounter.Text = kkCount.ToString();
+                SetValues();
                 SetCrateName();
+                //SetValues();
 
             }
 
         }
 
+        private void btnInvalidVoteIncrease_Clicked(object sender, EventArgs e)
+        {
+            if(currentVote > 0)
+            {
+                invalidVotes++;
+                Preferences.Set("invalidVotes", invalidVotes.ToString());
+                lblInvalidVotes.Text = invalidVotes.ToString();
+                currentVote--;
+                Preferences.Set("currentVote", currentVote.ToString());
+                lblVotesLeft.Text = currentVote.ToString();
+            }
+           
+        }
 
+        private void btnInvalidVoteDecrease_Clicked(object sender, EventArgs e)
+        {
+            if (invalidVotes > 0)
+            {
+                invalidVotes--;
+                Preferences.Set("invalidVotes", invalidVotes.ToString());
+                lblInvalidVotes.Text = invalidVotes.ToString();
+                currentVote++;
+                Preferences.Set("currentVote", currentVote.ToString());
+                lblVotesLeft.Text = currentVote.ToString();
+            }
+        }
 
+        private void btnVibrate_Clicked(object sender, EventArgs e)
+        {
+            vibrate = !vibrate;
+            Preferences.Set("vibrate", vibrate.ToString());
+            
+        }
 
     }
 }
